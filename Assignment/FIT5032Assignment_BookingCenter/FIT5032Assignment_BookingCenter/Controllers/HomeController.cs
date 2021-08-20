c1956c288e4144095e15b21ac43de22fe837035a
Shain001 <zsyycg@gmail.com>
2021-08-20 19:14:24 +1000

    Basic Page Structure and partial css/html complete

---
+++ Assignment/FIT5032Assignment_BookingCenter/FIT5032Assignment_BookingCenter/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032Assignment_BookingCenter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            ViewBag.Message = "This is service";
+            return View();
+        }
+
+        public ActionResult Book()
+        {
+            ViewBag.Message = "This is Book";
+            return View();
+        }
+
+
+    }
+}
---
+++ Assignment/FIT5032Assignment_BookingCenter/FIT5032Assignment_BookingCenter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032Assignment_BookingCenter.MvcApplication" Language="C#" %>
