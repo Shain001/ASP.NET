c9f5a8a17090e3b200371eb617db749e9b045deb
Shain001 <zsyycg@gmail.com>
2021-09-02 12:42:47 +1000

    week6&5

---
+++ week5/FIT5032_MyJS/FIT5032_MyJS/Models/DatePicker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_MyJS.Models
+{
+    public class DatePicker
+    {
+        public DateTime dateTime { get; set; }
+    }
+}
