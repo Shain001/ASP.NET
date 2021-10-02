ebbfe6650bf24d92decda26c3705d9b2845c79e5
Shain001 <zsyycg@gmail.com>
2021-10-02 20:55:54 +1000

    c level done, email done

--- Assignment/Assignment/Assignment/Controllers/HomeController.cs
+++ Assignment/Assignment/Assignment/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assignment.Models;
+using Assignment.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,5 +28,41 @@ namespace Assignment.Controllers
 
             return View();
         }
+
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+
+                    EmailSender es = new EmailSender();
+                    es.Send(toEmail, subject, contents);
+
+                    ViewBag.Result = "Email has been send.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
     }
+
 }
