87856b6cc65b53cec3d491bb2c9115334c01c220
Shain001 <zsyycg@gmail.com>
2021-10-11 21:57:09 +1100

    email improved

---
+++ Assignment/Assignment/Assignment/Utils/SubjectList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment.Utils
+{
+    public static class SubjectList
+    {
+        public static IEnumerable<SelectListItem> SubjectItems()
+        {
+            var itmes = new List<SelectListItem>
+            {
+                new SelectListItem() {Text="Complaint", Value = "Complaint"},
+                new SelectListItem() {Text="Feedback", Value="Feedback"},
+                new SelectListItem() {Text="General Question", Value="General Question"}
+
+            };
+            return itmes;
+        }
+    }
+}
